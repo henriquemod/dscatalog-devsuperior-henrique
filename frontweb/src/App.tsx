403206805e452d5186bc87aa431a178d02afeefa
Henrique Souza <henrique.asouza@outlook.com>
2021-12-25 13:33:57 -0300

    Initial home page

--- frontweb/src/App.tsx
+++ frontweb/src/App.tsx
@@ -1,14 +1,9 @@
 import './assets/styles/custom.scss';
 import './App.css';
-import Navbar from './components/Navbar';
+import Home from './pages/Home';
 
 function App() {
-  return (
-    <>
-      <Navbar />
-      <h1>Hello DSCatalog</h1>
-    </>
-  );
+  return <Home />;
 }
 
 export default App;
