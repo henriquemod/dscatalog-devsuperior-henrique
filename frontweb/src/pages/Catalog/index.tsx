f59f1901b681e535e0b92f6858cfd9510ecb1e19
Henrique Souza <henrique.asouza@outlook.com>
2021-12-26 20:01:17 -0300

    Props ProductCard, ProducePrice

--- frontweb/src/pages/Catalog/index.tsx
+++ frontweb/src/pages/Catalog/index.tsx
@@ -1,23 +1,44 @@
+import { Product } from 'types/product';
 import ProductCard from '../../components/ProductCard';
 
 const Catalog = () => {
+  const product: Product = {
+    id: 2,
+    name: 'Smart TV',
+    description: 'Descrição qualquer',
+    price: 2190.15,
+    imgUrl:
+      'https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/2-big.jpg',
+    date: '2020-07-14T10:00:00Z',
+    categories: [
+      {
+        id: 1,
+        name: 'Livros',
+      },
+      {
+        id: 3,
+        name: 'Computadores',
+      },
+    ],
+  };
+
   return (
     <div className="container my-4">
       <div className="row">
         <div className="col-sm-6 col-lg-4 col-xl-3">
-          <ProductCard />
+          <ProductCard product={product} />
         </div>
         <div className="col-sm-6 col-lg-4 col-xl-3">
-          <ProductCard />
+          <ProductCard product={product} />
         </div>
         <div className="col-sm-6 col-lg-4 col-xl-3">
-          <ProductCard />
+          <ProductCard product={product} />
         </div>
         <div className="col-sm-6 col-lg-4 col-xl-3">
-          <ProductCard />
+          <ProductCard product={product} />
         </div>
         <div className="col-sm-6 col-lg-4 col-xl-3">
-          <ProductCard />
+          <ProductCard product={product} />
         </div>
       </div>
     </div>
