5bcf204ce1dd5981e045b2f712717483d834fb94
Henrique Souza <henrique.asouza@outlook.com>
2021-12-25 21:02:16 -0300

    Home styling

--- frontweb/src/pages/Home/index.tsx
+++ frontweb/src/pages/Home/index.tsx
@@ -1,6 +1,7 @@
 import Navbar from '../../components/Navbar';
 import { ReactComponent as MainImage } from 'assets/images/main-image.svg';
 import './styles.css';
+import ButtonIcon from '../../components/ButtonIcon';
 
 const Home = () => {
   return (
@@ -9,7 +10,14 @@ const Home = () => {
       <div className="home-container">
         <div className="home-card">
           <div className="home-content-container">
-            <h1>Conheça o melhor catalogo de produtos</h1>
+            <div>
+              <h1>Conheça o melhor catalogo de produtos</h1>
+              <p>
+                Ajudaremos você a encontrar os melhores produtos disponíveis no
+                mercado.
+              </p>
+            </div>
+            <ButtonIcon />
           </div>
           <div className="home-image-container">
             <MainImage />
