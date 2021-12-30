f9fc9552db3bd756920f7a0e7b3e101af761f123
Henrique Souza <henrique.asouza@outlook.com>
2021-12-30 19:09:40 -0300

    Form validation

--- frontweb/src/pages/Admin/Auth/Login/index.tsx
+++ frontweb/src/pages/Admin/Auth/Login/index.tsx
@@ -11,15 +11,14 @@ type FormData = {
 };
 
 const Login = () => {
-  const { register, handleSubmit } = useForm<FormData>();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormData>();
   const [hasError, setHasError] = useState(false);
 
   const onSubmit = async (formData: FormData) => {
-    if (formData.username.length === 0 || formData.password.length === 0) {
-      setHasError(true);
-      return;
-    }
-
     try {
       const loginRequest = await requestBackendLogin(formData);
       console.log(loginRequest);
@@ -39,21 +38,33 @@ const Login = () => {
       <form onSubmit={handleSubmit(onSubmit)}>
         <div className="mb-4">
           <input
-            {...register('username')}
+            {...register('username', {
+              required: 'Campo obrigatorio',
+              pattern: {
+                value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+.[A-Z]{2,}$/i,
+                message: 'Email invalido',
+              },
+            })}
             type="text"
             className="form-control base-input"
             placeholder="Email"
             name="username"
           />
+          <div className="invalid-feedback d-block">
+            {errors.username?.message}
+          </div>
         </div>
         <div className="mb-2">
           <input
-            {...register('password')}
+            {...register('password', { required: 'Campo obrigatorio' })}
             type="password"
             className="form-control base-input "
             placeholder="Password"
             name="password"
           />
+          <div className="invalid-feedback d-block">
+            {errors.password?.message}
+          </div>
         </div>
         <Link to="/admin/auth/recover" className="login-link-recover">
           Esqueci a senha
