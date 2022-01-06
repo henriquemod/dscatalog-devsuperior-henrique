c624d2ce117cb024af2b4aef15082344d977fc15
Henrique Souza <henrique.asouza@outlook.com>
2022-01-06 09:13:06 -0300

    Refactor toke, Role

--- frontweb/src/pages/Admin/Auth/Login/index.tsx
+++ frontweb/src/pages/Admin/Auth/Login/index.tsx
@@ -6,7 +6,8 @@ import { useContext, useState } from 'react';
 import { AuthContext } from 'AuthContext';
 import './styles.css';
 import { saveAuthData } from 'util/storage';
-import { getTokenData, isAuthenticated } from 'util/auth';
+import { isAuthenticated } from 'util/auth';
+import { getTokenData } from 'util/token';
 
 type CredentialsDTO = {
   username: string;
